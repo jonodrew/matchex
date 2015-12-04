59bb0519c95bc8f1050fa78e3b3202277e639856
Jonathan Kerr <jonathandrewkerr@googlemail.com>
2015-12-04 07:19:04 +0000

    Created matrix to hold score

--- test1.py
+++ test1.py
@@ -2,23 +2,27 @@ import csv
 
 with open('postings.csv','rb') as f:
     reader = csv.reader(f)
-    preferences = list(reader)
+    postings = list(reader)
     #print(preferences)
 
 with open('candidates.csv','rb') as f:
     reader = csv.reader(f)
     candidates = list(reader)
     #print(candidates)
+#for values in candidates:
 
+
+matrix = []
 def match(p,c):
     score = 0
-    #p1 = preferences[0][0]
-    #c1 = candidates[8][25]
     if p == c:
         score += 1
     else:
         score
-    print(score)
-p = preferences[0][0]
-c = candidates[8][25]
-match(p,c)
+    matrix.append(score)
+
+p = postings[0][1]
+c = candidates[7][8]
+for list in postings:
+        match(p,c)
+print(matrix)
